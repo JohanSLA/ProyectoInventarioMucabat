c677df3608faf52638c0820170263904a43c9e0d
JohanSLA <johans.londonoa@uqvirtual.edu.co>
2024-11-17 17:01:13 -0500

    healt ok en local v1.7

--- database/db_Login/db.js
+++ database/db_Login/db.js
@@ -7,27 +7,50 @@
 //Invocamos a mysql el cual nos servira para establecer la conexion con la base de datos
 const mysql = require('mysql2');
 
+const { Pool } = require('pg');
 
-//Establecemos las variables para la conexion
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST || 'postgresql://root:EKv8cL8ChenMrY5Xn4l9FvzBzIHA1SGQ@dpg-cst3oqhu0jms73egr8sg-a/db_qlpk' ,
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'EKv8cL8ChenMrY5Xn4l9FvzBzIHA1SGQ' ,
-    database: process.env.DB_DATABASE || 'db_qlpk'
+const pool = new Pool({
+    connectionString: process.env.DB_CONNECTION_STRING || 'postgresql://root:EKv8cL8ChenMrY5Xn4l9FvzBzIHA1SGQ@dpg-cst3oqhu0jms73egr8sg-a/db_qlpk',
+    ssl: {
+        rejectUnauthorized: false // Para conexiones seguras en entornos de producción
+    }
 });
 
-//Ejecutamos la conexión, en caso de ocurrir un error; imprimimos un error por consola
-connection.connect((error)=>{
+//Establecemos las variables para la conexion
+//const connection = mysql.createConnection({
+    //host: process.env.DB_HOST || 'postgresql://root:EKv8cL8ChenMrY5Xn4l9FvzBzIHA1SGQ@dpg-cst3oqhu0jms73egr8sg-a/db_qlpk' ,
+    //user: process.env.DB_USER || 'root',
+    //password: process.env.DB_PASSWORD || 'EKv8cL8ChenMrY5Xn4l9FvzBzIHA1SGQ' ,
+   // database: process.env.DB_DATABASE || 'db_qlpk'
+//});
 
-    //Fragmento de codigo que se ejecutara en caso de ocurrir un error
-    if(error){
-        console.log('Ocurrio el siguiente error al intentar establecer conexión con la base de datos: '+error);
+
+// Probar la conexión
+pool.connect((error, client, release) => {
+    if (error) {
+        // Fragmento que se ejecutará si ocurre un error
+        console.log('Ocurrió el siguiente error al intentar establecer conexión con la base de datos:', error);
         return;
     }
 
-    console.log('Base de datos conectada con exito');
+    console.log('Base de datos conectada con éxito');
 
+    // Libera el cliente después de la prueba de conexión
+    release();
 });
+
+//Ejecutamos la conexión, en caso de ocurrir un error; imprimimos un error por consola
+//connection.connect((error)=>{
+
+    //Fragmento de codigo que se ejecutara en caso de ocurrir un error
+    //if(error){
+        //console.log('Ocurrio el siguiente error al intentar establecer conexión con la base de datos: '+error);
+       // return;
+   // }
+
+    //console.log('Base de datos conectada con exito');
+
+//});
 
 
 //Exportamos el modulo para poder usarlo en otro lado con el nombre de connection
